b1d053d980380aa71f26694c22749663ed25bb22
Quinten Kent <berlinquin@gmail.com>
2019-12-07 18:28:11 -0600

    Call yylex to populate tape

--- uno.cpp
+++ uno.cpp
@@ -14,10 +14,52 @@ int main(int argc, char *argv[])
    // The program stack
    std::stack<card_t> stack;
 
-   card_t next_card;
+   // The different modes the head can be in
+   enum { READ, SCAN } mode;
+   mode = READ;
 
-   while (yylex(&next_card) != 0)
+   // The jump direction
+   enum { LEFT, RIGHT } seek_direction;
+   seek_direction = RIGHT;
+
+   card_t next_card;
+   /*
+    * Invariants:
+    * head >= 0
+    */
+   while (true)
    {
+      // Read the symbol under the head
+      if (head == tape.size())
+      {
+         // Need to read in the next symbol
+         if (yylex(&next_card == 0);
+         {
+            printf("Reached end of input\n");
+            break;
+         }
+         tape.push_back(next_card);
+      }
+      else
+      {
+         // Symbol already in memory
+         next_card = tape.at(head);
+      }
+
+      // Handle the card based on the current mode
+      if (mode == READ)
+      {
+         // Examine the top of the stack
+         card_t top_card;
+         if (stack.empty())
+         {
+            top_card.type = WILD;
+         }
+         else
+         {
+            top_card = stack.pop();
+         }
+      }
 
    }
 
